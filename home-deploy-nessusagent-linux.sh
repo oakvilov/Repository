dd0d09f9aba72e29e671e37b4450a970662e7490
oakvilov <76740811+oakvilov@users.noreply.github.com>
2020-12-29 18:03:14 +0200

    Update home-deploy-nessusagent-linux.sh

--- home-deploy-nessusagent-linux.sh
+++ home-deploy-nessusagent-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sudo bash
 # Installation Nessus Agent v.7.6.3
 # Variables
-nessus_installer_url="https://github.com/oakvilov/Repository/blob/main/NessusAgent-8.2.2-ubuntu1110_amd64.deb"
+nessus_installer_url="https://github.com/oakvilov/Repository/raw/main/NessusAgent-8.2.2-ubuntu1110_amd64.deb"
 nessus_key="be6e817a936774295bcda98fd1dc6f3c8238d2b6fae7ae5bfeea7925689d83c8"
 nessus_host="nessus.levi9.com --port=8834"
 nessus_group="Ukraine-Clients"
